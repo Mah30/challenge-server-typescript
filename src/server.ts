f2df086d241cedebf1e3eb3a7eda4e79f2c2d6ef
Mariah <alinemariah@hotmail.com>
2025-04-15 14:50:00 +0200

    feat: clean code

--- src/server.ts
+++ src/server.ts
@@ -28,31 +28,84 @@ app.use(express.json());
 //app.use("/auth", authRouter); //Router ou Routes??
 //app.use("/api", router);
 app.use((req, res) => {
-  res.status(404).json({ message: "Endpoint not found" });
+  res.status(404).json({ 
+    message: "Endpoint not found",
+    suggestion: "Check the URL or go to Home" });
 });
+
 
 app.get("/", (req, res) => {
   res.send("Challenge Generator API is runnig! 🚀");
 });
 
 
-
-
 // Função assíncrona para inicialização
 
-/* async function main() {
-  try {
-    // Criar um usuário de exemplo
-    const user = await prisma.user.create({
-      data: {
-        username: "admin",
-        password: await bcrypt.hash("admin", 10),
-      },
-  
-    }); */
+async function createAdminIfNotExists(adminEmail: string, adminPassword: string) {
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(adminPassword, salt);
+
+  await prisma.user.create({
+    data: {
+      firstName: "Admin",
+      lastName: "User",
+      email: adminEmail,
+      passwordHash,
+      isAdmin: true, 
+    },
+  });
+
+  console.log(`✅ Admin user created: ${adminEmail}`);
+};
+
+async function ensureAdminExists(/* params:type */) {
+  const existingAdmin = await prisma.user.findFirst({where: {isAdmin: true}});
+  if (!existingAdmin) {
+    await createAdminIfNotExists("admin@example.com", "admin123"); 
+  }  
+}
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 
+
+
+
+
+
+// another art to write the code above - no cleancode
+/* async function createAdminIfNotExists() {
+  const adminEmail = "admin@example.com";
+  const adminPassword = "admin124";
+
+  const existingAdmin = await prisma.user.findFirst({
+    where: {isAdmin: true}
+  });
+
+  
+  if (!existingAdmin) {
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(adminPassword, salt);
+
+    await prisma.user.create({
+      data: {
+        firstName: "Admin",
+        lastName: "User",
+        email: adminEmail,
+        passwordHash,
+        isAdmin: true, 
+      },
+    });
+    console.log(`✅ Admin user created: ${adminEmail}`);
+  } else{
+    console.log("✅ An Admin user already exists.");
+  } 
+};
+
+createAdminIfNotExists()
+.catch((error) => {
+  console.log("❌ Error ensuring admin user:", error)
+}) */
